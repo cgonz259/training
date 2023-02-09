0c1c470f972d8cd73e6ac0130dc895e71cde0822
Camila González <gonzalezzccamila@gmail.com>
2023-02-09 09:27:35 -0300

    a

---
+++ resumen.js
@@ -0,0 +1,61 @@
+// // VARIABLES********************************************
+// var let y const son para poner variables
+// //le asignas un valor a la variable cuando la declaras
+// let carName = "Ecosprt"; // variable carName, valor Ecosport
+// // Then we "output" the value inside an HTML paragraph with id="demo"
+// document.getElementById("demo").innerHTML = carName; //
+// // se puede usar una sola declaracion para muchas variables. sin el innerhtml no funciona no se por q 
+// let person = "Cam", //COMA AL FINAL PARA SEPARAR
+// carName = "ecosport",
+// price = 4; //cdo terminas pones ;
+// //USAR LET Y CONS//////////////////////////////////////////
+
+
+
+
+
+
+
+// //FUNCIONES**************************************
+
+// function myfirstFun (p1, p2) {
+//     return p1*p2
+// }
+// //a la funcion le das 2 valores, y te devuelve en producto entre las mismas
+// // o sea las declaras pero no lo muestra, para que lo muestre tenes que poner  
+
+// document.getElementById("demo").innerHTML = myFunction(4, 3); // de donde sale el id??? 
+// <p id="demo"></p> // se declara al principio, sino no funciona
+
+   
+
+// ///////////////////////
+// function logPlayer (name){
+//     console.log('New game starting for player : ${name}'); //ponemos comillas simples y ahi adentro queda con el $ el nombre del parámetro que pusimos nosotros
+// }
+
+// //////////
+// como crear un objeto
+// ///////////objectliteral no se q es pero ok
+// const objLiteral = {
+//     balance : 500
+// };
+
+// PARA USARLA = ${objLiteral.balance}
+// //////////usar una class
+// class classObject {
+//     balance = 1000;
+// }
+// const cla   ssObj = new classObject();
+
+
+// PARA USARLA = ${classObj.balance}
+
+// ////////create using a function
+// function functionObject(){
+//     this.balance = 9000;
+// }
+// const functionObj = new functionObject();
+
+// autenticacion : tener tu token e identificarte
+// autorizacion : pertenece a x grupo o x rol
